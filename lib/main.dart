3e691951c62d0d5739da55396c35679dfd671a59
Nino <1023954189@qq.com>
2018-12-24 18:03:27 +0800

    commit test 1

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Test Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -25,38 +25,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text("listview"),
+            onTap: () {
+              Navigator.of(context).push(new MaterialPageRoute(builder: (context){
+
+              }));
+            },
+          ),
+          new ListTile(
+            title: new Text("listview加载特效"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("gridview"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("listview+gridview"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("scrollview+listview"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("弹窗"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("网络请求1"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("异步"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("网络请求2"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("json解析"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("json解析"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("文本输入"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("图片"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("文件存储+sqlite"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("cupertino"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("viewpager"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("导航路由"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("gestures手势"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("动画"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("画笔"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("自定义动画"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("生命周期"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("多语言"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("主题配置"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("插件1"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("插件2"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("插件3"),
+            onTap: () {},
+          ),
+          new ListTile(
+            title: new Text("插件4"),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
---
+++ lib/page1/Page1.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MyListView1();
+  }
+
+  /// listview.builder
+
+  Widget MyListView1() {
+    return null;
+  }
+
+  /// listview.custom
+  Widget MyListView2() {
+    return null;
+  }
+
+  /// listview.separated
+  Widget MyListView3() {
+    return null;
+  }
+}
