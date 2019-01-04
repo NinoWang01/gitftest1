66375cf0cb3db43e81fa90436db5c7012b99c258
Nino <1023954189@qq.com>
2019-01-04 15:21:04 +0800

    弹窗

--- lib/page6/Page6.dart
+++ lib/page6/Page6.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class Page6 extends StatefulWidget {
   @override
@@ -37,24 +38,40 @@ class _Page6State extends State<Page6> {
                     });
               }),
           RaisedButton(
-              child: new Text("tc3"),
+              child: new Text("底部弹窗"),
               onPressed: () {
+//                return showBottomSheet(context: context, builder: (context) {
                 tc3();
+//                });
               }),
+          Builder(
+            builder: (context) {
+              return RaisedButton(
+                  child: new Text("snackbar"),
+                  onPressed: () {
+                    tc4(context);
+                  });
+            },
+          ),
           RaisedButton(
-              child: new Text("tc4"),
-              onPressed: () {
-                tc4();
-              }),
-          RaisedButton(
-              child: new Text("tc5"),
+              child: new Text("cupertino底部弹窗"),
               onPressed: () {
                 tc5();
               }),
           RaisedButton(
-              child: new Text("tc6"),
+              child: new Text("CupertinoActionSheet"),
               onPressed: () {
                 tc6();
+              }),
+          RaisedButton(
+              child: new Text("CupertinoAlertDialog"),
+              onPressed: () {
+                tc7();
+              }),
+          RaisedButton(
+              child: new Text("CupertinoDialog"),
+              onPressed: () {
+                tc8();
               }),
         ],
       ),
@@ -117,11 +134,117 @@ class _Page6State extends State<Page6> {
     );
   }
 
-  tc3() {}
+  tc3() {
+    return showModalBottomSheet<Null>(
+        context: context,
+        builder: (BuildContext context) {
+          return new Container(
+              child: new Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: new Text('这是模态底部面板，点击任意位置即可关闭',
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                          color: Theme.of(context).accentColor,
+                          fontSize: 24.0))));
+        });
+  }
 
-  tc4() {}
+  tc4(context) {
+    Scaffold.of(context).showSnackBar(new SnackBar(
+      content: new Text('这是一个SnackBar'),
+      action: SnackBarAction(
+          label: '撤销',
+          onPressed: () {
+            print('-------------撤销');
+          }),
+    ));
+  }
 
-  tc5() {}
+  tc5() {
+    return showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return new Container(
+              child: new Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: new Text('这是模态底部面板，点击任意位置即可关闭',
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                          color: Theme.of(context).accentColor,
+                          fontSize: 24.0))));
+        });
+  }
 
-  tc6() {}
+  tc6() {
+    return showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return CupertinoActionSheet(
+            title: new Text('title'),
+            message: new Text('message'),
+            actions: <Widget>[
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.of(context).pop(context);
+                  },
+                  child: Text("11111")),
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.of(context).pop(context);
+                  },
+                  child: Text("22222")),
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.of(context).pop(context);
+                  },
+                  child: Text("33333")),
+              CupertinoActionSheetAction(
+                  onPressed: () {
+                    Navigator.of(context).pop(context);
+                  },
+                  child: Text("44444")),
+            ],
+            cancelButton: new CupertinoActionSheetAction(
+                onPressed: () {
+                  Navigator.of(context).pop(context);
+                },
+                child: Text("cancelButton")),
+          );
+        });
+  }
+
+  tc7() {
+    return showCupertinoDialog(context: context, builder: (coontext){
+     return CupertinoAlertDialog(
+        title: new Text("title"),
+        content: new Text('contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent'),
+        actions: <Widget>[
+          CupertinoDialogAction(
+         isDestructiveAction:true,//此动作是否会破坏对象。
+            child: Text("取消"),
+            onPressed: () {
+              Navigator.of(context).pop(context);
+            },
+          ),
+          CupertinoDialogAction(
+            child: Text("确定"),
+            onPressed: () {
+              Navigator.of(context).pop(context);
+            },
+          ),
+
+        ],
+      );
+    });
+  }
+
+  tc8() {
+    return showCupertinoDialog(context: context, builder: (context){
+      return CupertinoDialog(
+        child: GestureDetector(child: new Text("111111"),onTap: (){
+          Navigator.of(context).pop(context);
+        },),
+      );
+    });
+  }
 }
